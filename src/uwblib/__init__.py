e90ababbf32d7b3c9c48749ccf49044628027d01
Laurence Liang <lliang@groq.com>
2025-07-28 00:21:48 -0400

    chore: wip

--- src/uwblib/__init__.py
+++ src/uwblib/__init__.py
@@ -1,2 +1,13 @@
-def main() -> None:
-    print("Hello from uwblib!")
+import scipy
+import numpy as np
+
+
+def get_stats(cir_data):
+    curr_dict = dict()
+
+    curr_dict["kurtosis"] = scipy.stats.kurtosis(cir_data)
+    curr_dict["variance"] = np.var(cir_data)
+    curr_dict["mean"] = np.mean(cir_data)
+    curr_dict["amplitude"] = np.max(np.fabs(cir_data))
+
+    return curr_dict
